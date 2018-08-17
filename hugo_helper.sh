a241d26e2a581826b3e584188df3359d35a7adcd
billryan <yuanbin2014@gmail.com>
2018-08-18 00:21:52 +0800

    update blog engine with use hugo even

---
+++ hugo_helper.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+year_month=$(date '+%Y-%m')
+full_date=$(date '+%Y-%m-%d_%H-%M-%S')
+hugo_fn=posts/${year_month}/${full_date}.md
+new_fn=content/${hugo_fn}
+fn=${new_fn}
+
+# hugo new posts//.md
+
+while getopts "o:nt:" opt; do
+  case $opt in
+    n)
+      echo "create new posts with name ${filename}"
+      hugo new ${hugo_fn}
+      ;;
+    o)
+      echo "replace old filename $OPTARG title..."
+      fn=content/$OPTARG
+      ;;
+    t)
+      echo "replace with title $OPTARG"
+      sed -i "s/^title: \".*\"$/title: \"$OPTARG\"/g" ${fn}
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
